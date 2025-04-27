a7c40e3d232761a5a9a7971c2d266bfe43f45866
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-04-27 21:02:35 +0800

    chore: created a input field component - initial

---
+++ src/components/utils/InputField.jsx
@@ -0,0 +1,28 @@
+export default function InputFields({
+  labelName,
+  inputType,
+  name,
+  options = [],
+  withLabel = true,
+}) {
+  return (
+    <>
+      {inputType === "select" ? (
+        <select
+          name={name}
+          id=""
+        ></select>
+      ) : inputType === "textarea" ? (
+        <textarea
+          name={name}
+          id=""
+        ></textarea>
+      ) : (
+        <div className="input__container">
+          {withLabel && <label htmlFor={name}>{labelName}</label>}
+          <input type={inputType} />
+        </div>
+      )}
+    </>
+  );
+}
