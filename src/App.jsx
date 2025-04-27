d5cb9483e71a96df987df5f7228b5d42892b45be
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-04-27 13:20:52 +0800

    feat: created and design the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import Header from "./components/layout/Header";
+
 export default function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="app">
+      <header className="header__container">
+        <Header />
+      </header>
+
+      <div className="main__content">
+        <nav className="nav__container">
+          {/* Call the sidebar componenet here */}
+        </nav>
+
+        <main className="cv__content--container">
+          {/* Form Sections and Preview Section*/}
+        </main>
+      </div>
+    </div>
   )
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header__contents container">
+      <h1>
+        <a href="">
+          Nex<span>tC</span>V
+        </a>
+      </h1>
+    </div>
+  )
+}
