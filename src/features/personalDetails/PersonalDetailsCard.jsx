44039b0fbb602a0194668179faf7ff3e12b188d3
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-04-29 00:07:25 +0800

    chore: add contents

--- src/features/personalDetails/PersonalDetailsCard.jsx
+++ src/features/personalDetails/PersonalDetailsCard.jsx
@@ -1,6 +1,15 @@
+import FetchPersonalDetails from "./FetchPersonalDetails";
+
 export default function PersonalDetailsCard() {
+  const { fetchedPersonalDetails, setFetchedPersonalDetails } = FetchPersonalDetails();
+
   return (
-    <>
-    </>
+    <div className="personal__details--card">
+      <h3>{fetchedPersonalDetails.full_name}</h3>
+      <p>{fetchedPersonalDetails.job_title}</p>
+      <p>{fetchedPersonalDetails.email}</p>
+      <p>{fetchedPersonalDetails.phone}</p>
+      <p>{fetchedPersonalDetails.address}</p>
+    </div>
   )
 }
