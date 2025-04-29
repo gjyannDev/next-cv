53323ce4eccd1f03c511d8dc2fd90b62f0c32ba8
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-04-29 21:35:37 +0800

    chore: add functionality when status is trigger call what value is select

--- src/features/technicalSkillsDetails/TechnicalSkillsMode.jsx
+++ src/features/technicalSkillsDetails/TechnicalSkillsMode.jsx
@@ -1,26 +1,31 @@
 import { useState } from "react";
 import InputFields from "../../components/utils/InputField";
+import LanguageSkillsForm from "./forms/LanguageSkillsForm";
 
 export default function TechnicalSkillsMode() {
   const [selectedMode, setSelectedMode] = useState("language");
 
   const select_values = [
-    { value: "language", label: "Programming Language"},
-    { value: "frameworks", label: "Frameworks, Libraries, and Databases"},
-    { value: "tools", label: "Tools & Other Technologies"}
-  ]
+    { value: "language", label: "Programming Language" },
+    { value: "frameworks", label: "Frameworks, Libraries, and Databases" },
+    { value: "tools", label: "Tools & Other Technologies" },
+  ];
 
   return (
     <div className="skills__details--container">
       <h1>Technical Skills</h1>
-      <div className="drop__down--container">
-        <InputFields
-          labelName={""}
-          inputType={"select"}
-          name={"skill"}
-          withLabel={false}
-          options={select_values}
-        />
+      <div className="content__form--container">
+        <div className="drop__down--container">
+          <InputFields
+            labelName={""}
+            inputType={"select"}
+            name={"skill"}
+            withLabel={false}
+            options={select_values}
+            onChange={setSelectedMode}
+          />
+        </div>
+        {selectedMode === "language" ? <LanguageSkillsForm /> : ""}
       </div>
     </div>
   );
---
+++ src/features/technicalSkillsDetails/forms/LanguageSkillsForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import InputFields from "../../../components/utils/InputField";
+
+export default function LanguageSkillsForm() {
+  const [languageInputs, setLanguageInputs] = useState(["", ""]);
+
+  return (
+    <div className="language__form--container">
+      <h3 className="form__title">{"Programming Languages"}</h3>
+      <div className="language__form--contents">
+        {languageInputs.map((language, index) => (
+          <div className={`language__input${index + 1} language__input--container`}>
+            <h3>{index + 1}</h3>
+            <InputFields
+              labelName={""}
+              name={"language"}
+              inputType={"input"}
+              value={""}
+              withLabel={false}
+            />
+            <button type="button" className="btn__remove btn--primary">Remove</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
