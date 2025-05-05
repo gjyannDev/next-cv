861ecaf886b49b6f6146d009532eadbcf59d96ef
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-05 08:41:13 +0800

    chore: refactored the common form used in feature pages

---
+++ src/components/common/FeaturePagesForm.jsx
@@ -0,0 +1,47 @@
+import InputFields from "../utils/InputField";
+
+export default function FeaturePagesForm({
+  handleOnSubmit,
+  inputDetails,
+  value,
+  handleOnChange,
+  setStatus,
+  status,
+}) {
+  return (
+    <div className="form__container">
+      {
+        <form onSubmit={handleOnSubmit}>
+          {inputDetails.map((input) => (
+            <InputFields
+              labelName={input.labelName}
+              inputType={input.inputType}
+              name={input.name}
+              value={value[input.name] || ""}
+              onChange={handleOnChange}
+            />
+          ))}
+
+          <div className="grouped__btn--container">
+            {status === "edit details" && (
+              <button
+                type="submit"
+                className="btn--primary-2"
+                onClick={() => setStatus("card")}
+              >
+                Cancel
+              </button>
+            )}
+
+            <button
+              type="submit"
+              className="btn--primary"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      }
+    </div>
+  );
+}
