8d57d57b316cfaa08dad24a345cc6ec86dc146f2
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-05 23:54:33 +0800

    chore: add work experience section in resume

--- src/features/previewPage/PreviewPage.jsx
+++ src/features/previewPage/PreviewPage.jsx
@@ -1,11 +1,15 @@
-import PersonalDetailsSection from "./resumeSections/PersonalDetailsSection"
-import EducationDetailsSection from "./resumeSections/educationDetailsSection"
+import PersonalDetailsSection from "./resumeSections/PersonalDetailsSection";
+import EducationDetailsSection from "./resumeSections/educationDetailsSection";
+import WorkExperienceSection from "./resumeSections/WorkExperienceSection";
 
-export default function PreviewPage({resumeData}) {
+export default function PreviewPage({ resumeData }) {
   return (
     <div className="preview__page--container">
-      <PersonalDetailsSection data={resumeData.personal}/>
-      <EducationDetailsSection data={resumeData.education}/>
+      <PersonalDetailsSection data={resumeData.personal} />
+      <div className="resume__body--contents">
+        <EducationDetailsSection data={resumeData.education} />
+        <WorkExperienceSection data={resumeData.workExperience} />
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/features/previewPage/resumeSections/WorkExperienceSection.jsx
@@ -0,0 +1,28 @@
+import { getYearDate } from "../../../services/cv/utils";
+
+export default function WorkExperienceSection({ data }) {
+  return (
+    <div className="work__details--section">
+      <h1 className="resume__header--font">Work Experience</h1>
+      <hr className="custom--line" />
+      <div className="work__card--container">
+        {data.map((work, index) => (
+          <div className={`work__${index}--container work__resume--card`}>
+            <div className="card__content--first">
+              <h3 className="school--text resume__header--font">
+                {work.company_name}
+              </h3>
+              <h4>{getYearDate(work.start_date, work.end_date)}</h4>
+            </div>
+            <div className="card__content--second">
+              <p className="resume__header--font degree--text">{work.job_title}</p>
+            </div>
+            <div className="card__content--third">
+              {/* <p>{work.description}</p> */}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
