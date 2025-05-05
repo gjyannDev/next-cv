f0657f107d58a774488beb8257d6441b3bec83ad
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-05 23:55:22 +0800

    chore: just refactored because of prettier

--- src/features/previewPage/resumeSections/educationDetailsSection.jsx
+++ src/features/previewPage/resumeSections/educationDetailsSection.jsx
@@ -4,12 +4,14 @@ export default function EducationDetailsSection({ data }) {
   return (
     <div className="education__details--section">
       <h1 className="resume__header--font">Education</h1>
-      <hr className="custom--line"/>
+      <hr className="custom--line" />
       <div className="education__cards--container">
         {data.map((educ, index) => (
           <div className={`education__${index}--container educ__resume--card`}>
             <div className="card__content--first">
-              <h3 className="school--text resume__header--font">{educ.school}</h3>
+              <h3 className="school--text resume__header--font">
+                {educ.school}
+              </h3>
               <h4>{getYearDate(educ.start_date, educ.end_date)}</h4>
             </div>
             <div className="card__content--second">
