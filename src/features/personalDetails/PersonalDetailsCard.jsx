3871f266dafd0f404382256eb27ec2da23ca2cab
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-07 21:00:13 +0800

    chore: added edit personal details and also preview details button

--- src/features/personalDetails/PersonalDetailsCard.jsx
+++ src/features/personalDetails/PersonalDetailsCard.jsx
@@ -1,15 +1,32 @@
 import FetchData from "../../services/cv/FetchData";
 
-export default function PersonalDetailsCard({setStatus}) {
+export default function PersonalDetailsCard({ setStatus }) {
   const { fetchedPersonalDetails } = FetchData();
-  
+
   return (
-    <div className="personal__details--card" onClick={() => { setStatus("edit") }}>
-      <h3>{fetchedPersonalDetails.full_name}</h3>
-      <p>{fetchedPersonalDetails.job_title}</p>
-      <p>{fetchedPersonalDetails.email}</p>
-      <p>{fetchedPersonalDetails.phone}</p>
-      <p>{fetchedPersonalDetails.address}</p>
-    </div>
-  )
+    <>
+      <div
+        className="personal__details--card"
+        onClick={() => {
+          setStatus("edit");
+        }}
+      >
+        <h3>{fetchedPersonalDetails.full_name}</h3>
+        <p>{fetchedPersonalDetails.job_title}</p>
+        <p>{fetchedPersonalDetails.email}</p>
+        <p>{fetchedPersonalDetails.phone}</p>
+        <p>{fetchedPersonalDetails.address}</p>
+      </div>
+      <button
+        type="button"
+        className="btn__black--outline"
+        style={{ alignSelf: 'center' }}
+        onClick={() => {
+          setStatus("add");
+        }}
+      >
+        + Personal Details
+      </button>
+    </>
+  );
 }
